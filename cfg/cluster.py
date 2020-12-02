f1aea2dab0c27e9c0906f925b880f24c48b7be0d
Zhucong Xi <zhucongx@umich.edu>
2020-12-02 01:25:37 -0500

    cluster class

---
+++ cfg/cluster.py
@@ -0,0 +1,14 @@
+from cfg.atom import Atom
+import typing
+
+
+class Cluster(object):
+    def __init__(self, *atoms: typing.Union[Atom, typing.List[Atom]]):
+        if len(atoms) == 1 and isinstance(atoms[0], list):
+            self.__atom_list: typing.List[Atom] = atoms[0]
+        else:
+            self.__atom_list: typing.List[Atom] = list()
+            for insert_atom in atoms:
+                self.__atom_list.append(insert_atom)
+
+        self.__atom_list.sort(key=lambda sort_atom: sort_atom.atom_id)
