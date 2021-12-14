63f5bab96766f2e8f89d850bd98d261c8bf79b5d
Zhucong Xi <zhucongx@umich.edu>
2021-12-13 21:19:01 -0500

    bk

--- build_database.py
+++ build_database.py
@@ -2,4 +2,4 @@ from configtools import *
 
 ansys.build_pd_file({"Al", "Mg", "Zn"},
                     "../all_data_neb_results/",
-                    "./data/processed/all_compiled_data_second.pkl")
+                    "./data/processed/all_compiled_data.pkl")
