68ae70d28ebcb65662e2809ba31bc868ceff5f91
Zhucong Xi <zhucongx@umich.edu>
2022-08-24 19:31:13 -0400

    Update build_database.py

--- build_database.py
+++ build_database.py
@@ -1,13 +1,13 @@
 from configtools import *
 
 ansys.build_pd_file({"Al", "Mg", "Zn"},
-                    "../all_data_neb_results/",
+                    "./data/raw/all_data_neb_results/",
                     "./data/processed/AlMgZn_compiled.pkl")
 
 # ansys.build_pd_file({"Al", "Mg", "Sn", "Zn"},
-#                     "../all_data_neb_results/",
+#                     "./data/raw/all_data_neb_results/",
 #                     "./data/processed/AlMgZn_compiled_q.pkl")
 
 # ansys.build_pd_file({"Al", "Mg", "Zn", "Sn"},
-#                     "../all_data_neb_results_sn/",
+#                     "./data/raw/all_data_neb_results_sn/",
 #                     "./data/processed/AlMgZnSn_compiled.pkl")
