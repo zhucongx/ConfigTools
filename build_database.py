a2df26589c491a2233512912a27bb6a68b172a1e
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2022-10-18 12:47:16 -0400

    1

--- build_database.py
+++ build_database.py
@@ -1,7 +1,7 @@
 from configtools import *
 
 ansys.build_pd_file({"Al", "Mg", "Zn"},
-                    "./data/raw/all_data_neb_results/",
+                    "./data/raw/all_data_neb/",
                     "./data/processed/AlMgZn_compiled.pkl")
 
 # ansys.build_pd_file({"Al", "Mg", "Sn", "Zn"},
