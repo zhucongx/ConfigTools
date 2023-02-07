ce9b02338e51d4ceb6c460a45cfd839c623726f0
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-02-07 15:55:12 -0500

    Update __init__.py

--- configtools/cfg/__init__.py
+++ configtools/cfg/__init__.py
@@ -1,7 +1,7 @@
 from configtools.cfg.constants import *
 from configtools.cfg.atomic_mass import get_atomic_mass
-from configtools.cfg.atom import Atom, get_average_fractional_position_atom, get_fractional_distance_vector, \
-    get_bond_length_type_between
+from configtools.cfg.atom import Atom, get_average_fractional_position_atom, \
+    get_fractional_distance_vector, get_bond_length_type_between
 from configtools.cfg.config import Config, read_config, write_config, read_poscar, write_poscar, \
     get_config_system, get_average_position_config, get_pair_center, get_pair_rotation_matrix, \
     get_neighbors_set_of_jump_pair, get_neighbors_set_of_atom, \
