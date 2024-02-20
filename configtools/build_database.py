4cf3f9c660cb0ebe52c7d428e6683276f680be28
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2024-02-20 14:29:03 -0500

    Update build_database.py

--- configtools/build_database.py
+++ configtools/build_database.py
@@ -1,21 +1,21 @@
 from configtools import *
 
-# ansys.build_pd_file({"Al", "Mg", "Zn"},
-#                     "../data/raw/all_data_neb_AlMgZn/",
-#                     "../data/processed/AlMgZn_compiled.pkl")
+ansys.build_pd_file({"Al", "Mg", "Zn"},
+                    "../data/raw/all_data_neb_AlMgZn/",
+                    "../data/processed/AlMgZn_compiled.pkl")
 
 
-# ansys.build_pd_file({"Al", "Cu", "Mg", "Zn"},
-#                     "../data/raw/all_data_neb_AlCuMgZn/",
-#                     "./data/processed/AlCuMgZn_compiled.pkl")
-# ansys.build_pd_file({"Al", "Cu", "Mg", "Zn"},
-#                     "../data/raw/all_data_neb_AlMgZn/",
-#                     "../data/processed/AlMgZn_Cu_compiled.pkl")
-#
-#
-# ansys.build_pd_file({"Al", "Mg", "Sn", "Zn"},
-#                     "../data/raw/all_data_neb_AlMgSnZn/",
-#                     "./data/processed/AlMgSnZn_compiled.pkl")
-# ansys.build_pd_file({"Al", "Mg", "Sn", "Zn"},
-#                     "../data/raw/all_data_neb_AlMgZn/",
-#                     "../data/processed/AlMgZn_Sn_compiled.pkl")
+ansys.build_pd_file({"Al", "Cu", "Mg", "Zn"},
+                    "../data/raw/all_data_neb_AlCuMgZn/",
+                    "./data/processed/AlCuMgZn_compiled.pkl")
+ansys.build_pd_file({"Al", "Cu", "Mg", "Zn"},
+                    "../data/raw/all_data_neb_AlMgZn/",
+                    "../data/processed/AlMgZn_Cu_compiled.pkl")
+
+
+ansys.build_pd_file({"Al", "Mg", "Sn", "Zn"},
+                    "../data/raw/all_data_neb_AlMgSnZn/",
+                    "./data/processed/AlMgSnZn_compiled.pkl")
+ansys.build_pd_file({"Al", "Mg", "Sn", "Zn"},
+                    "../data/raw/all_data_neb_AlMgZn/",
+                    "../data/processed/AlMgZn_Sn_compiled.pkl")
